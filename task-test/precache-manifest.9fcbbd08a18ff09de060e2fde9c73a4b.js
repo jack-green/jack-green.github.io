30c5a2bb1f2383d332b222626e55528fa2146f49
Jack Green <jack@zegreen.com>
2018-12-05 22:34:50 +0000

    Updated paths

---
+++ task-test/precache-manifest.9fcbbd08a18ff09de060e2fde9c73a4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "c6fc970572a586322c61",
+    "url": "./static/js/main.c6fc9705.chunk.js"
+  },
+  {
+    "revision": "a4001cb7dd9dc0ba9b9c",
+    "url": "./static/js/1.a4001cb7.chunk.js"
+  },
+  {
+    "revision": "c6fc970572a586322c61",
+    "url": "./static/css/main.8f79504c.chunk.css"
+  },
+  {
+    "revision": "cf91fab1e224da65d49726f51d605b3e",
+    "url": "./index.html"
+  }
+];
