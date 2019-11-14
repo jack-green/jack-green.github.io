335327a5e835172a1e0c1b2e06930d215fb1b3ad
Jack <jack@monkii.com>
2019-11-14 11:53:17 +1100

    Updated semaphore with react version

---
+++ semaphore/precache-manifest.661dd8b72383fe60db4c9a1fd7f47a07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc2d7f657aaa684d1de8ffb1e2caa9fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd073d19bc8a2e2a8937",
+    "url": "/static/css/main.63da24c8.chunk.css"
+  },
+  {
+    "revision": "f9ada8d0bf6fe5e41476",
+    "url": "/static/js/2.c4eea5ea.chunk.js"
+  },
+  {
+    "revision": "cd073d19bc8a2e2a8937",
+    "url": "/static/js/main.3684b24d.chunk.js"
+  },
+  {
+    "revision": "72c906e6bb959e3c461e",
+    "url": "/static/js/runtime-main.73cf9429.js"
+  }
+]);
